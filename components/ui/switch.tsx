a820e1c421a96a83376081e21b9e5794c6eba984
osamamalikawan <osama.1@yahoo.com>
2026-09-21 13:40:39 +0500

    Update

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -12,7 +12,7 @@ export function Switch({ checked, onChange }: { checked: boolean; onChange: (v: 
       }`}
     >
       <span
-        className={`absolute top-[2px] h-[16px] w-[16px] rounded-full transition-transform ${
+        className={`absolute top-[2px] h-[16px] w-[16px] left-[2px] rounded-full transition-transform ${
           checked ? "translate-x-[18px] bg-basil-500" : "translate-x-[2px] bg-ink-faint"
         }`}
       />
